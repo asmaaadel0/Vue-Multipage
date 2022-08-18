c65c8e9f251d418c7e8b9d640386ae958fa5ae1c
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-18 12:39:24 +0200

    update navbar links

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation></the-navigation>
   <main>
     <router-view></router-view>
   </main>
@@ -16,7 +16,6 @@ export default {
   },
   data() {
     return {
-      activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -36,11 +35,6 @@ export default {
       teams: this.teams,
       users: this.users,
     };
-  },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
   },
 };
 </script>
