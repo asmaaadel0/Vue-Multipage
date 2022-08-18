24ca2103a1618c7f9dd10735bbc284df2d04a052
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-18 11:53:36 +0200

    add html and css codes

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <the-navigation @set-page="setActivePage"></the-navigation>
+  <main>
+    <component :is="activePage"></component>
+  </main>
+</template>
+
+<script>
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TheNavigation from './components/nav/TheNavigation.vue';
+
+
+export default {
+  components: {
+    TheNavigation,
+    TeamsList,
+    UsersList,
+  },
+  data() {
+    return {
+      activePage: 'teams-list',
+      teams: [
+        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
+        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'asmaa adel', role: 'Engineer' },
+        { id: 'u2', fullName: 'alaa emad', role: 'Engineer' },
+        { id: 'u3', fullName: 'esraa khaled', role: 'Engineer' },
+        { id: 'u4', fullName: 'hasnaa mohamed', role: 'Consultant' },
+        { id: 'u5', fullName: 'mai adham', role: 'Consultant' },
+      ],
+    };
+  },
+  provide() {
+    return {
+      teams: this.teams,
+      users: this.users,
+    };
+  },
+  methods: {
+    setActivePage(page) {
+      this.activePage = page;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
