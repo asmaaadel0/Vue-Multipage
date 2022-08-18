1f0935699d0832d87db8d12795e48f4f2a5aab66
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-18 12:43:28 +0200

    update styling

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const router = createRouter({
     { path: "/teams", component: TeamsList },
     { path: "/users", component: UsersList },
   ],
+  linkActiveClass: "active",
 });
 
 const app = createApp(App);
