c9e8e3c6d64f4533c8788a9a520e5958dbb6f6f6
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-18 14:22:51 +0200

    add footer

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from "./components/teams/TeamsList.vue";
 import UsersList from "./components/users/UsersList.vue";
 import TeamMembers from "./components/teams/TeamMembers.vue";
 import NotFound from "./components/nav/NotFound.vue";
+import TeamsFooter from "./components/teams/TeamsFooter.vue";
+import UsersFooter from "./components/users/UsersFooter.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,7 +17,7 @@ const router = createRouter({
     {
       name: "teams",
       path: "/teams",
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
           name: "team-members",
@@ -25,7 +27,7 @@ const router = createRouter({
         },
       ],
     },
-    { path: "/users", component: UsersList },
+    { path: "/users", components: { default: UsersList, footer: UsersFooter } },
 
     { path: "/:notFound(.*)", component: NotFound },
   ],
